d9307cae6397f1a2c76af7b832f88bba15cd3996
IoDmitri <Dmitri.Io@utexas.edu>
2017-01-06 23:12:56 -0800

    Added reddit_dataset_maker

--- reddit_dataset_maker.py
+++ reddit_dataset_maker.py
@@ -1,7 +1,7 @@
 from language_model import Language_model 
 
 def gen_reddit_model():
-	model = Language_model(batch_size=200, max_steps=25, save_dir="reddit", num_layers=2)
+	model = Language_model(batch_size=100, max_steps=25, save_dir="reddit", num_layers=2)
 	f_name = "./reddit_data/data.txt"
 	model.train_on_file(f_name)
 
