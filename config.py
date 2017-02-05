d2ace5cd50597c64b3c11b63f64cb1c54bcd8564
IoDmitri <Dmitri.Io@utexas.edu>
2017-02-05 15:55:08 -0800

    Added more files

---
+++ config.py
@@ -0,0 +1,26 @@
+class Config(object):
+	def __init__(
+		self, 
+		num_layers=1, 
+		device='gpu', 
+		batch_size=64, 
+		embed_size=100, 
+		hidden_size=100, 
+		dropout=0.90, 
+		max_steps=45, 
+		max_epochs=10, 
+		lr=0.0001,
+		cell="gru",
+		name="language_model"
+	   ):
+		self.num_layers=num_layers
+		self.device=device
+		self.batch_size=batch_size
+		self.embed_size=embed_size
+		self.hidden_size=hidden_size
+		self.dropout=dropout
+		self.max_epochs=max_epochs
+		self.max_steps=max_steps
+		self.lr=lr
+		self.cell=cell
+		self.name="language_model"
